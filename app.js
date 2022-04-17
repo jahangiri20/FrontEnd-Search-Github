fee0bf70c512921e7a0b9aa2dac4a882770cd595
hadis jahangiri <jahangiriweb@gmail.com>
2022-04-17 17:48:26 +0430

    v5

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const btnSearch = document.querySelector(".btn__Search");
 const inputName = document.querySelector("#inputName");
 const imgAvatar = document.querySelector("#avatar");
 const showUser = document.querySelector(".show__user");
+const showName = document.querySelector(".show__name")
 const showUerLogin = document.querySelector(".show__user--login");
 const locations = document.querySelector(".locations");
 const post = document.querySelector(".post");
 const userFollowing = document.querySelector(".user__Following");
 const userFollowers = document.querySelector(".user__Followers");
 const apiShow = document.querySelector(".api__show");
-
-
-
 
 
 btnSearch.addEventListener ("click", ()=>{
@@ -28,16 +26,15 @@ btnSearch.addEventListener ("click", ()=>{
     .then((data) => {
         render(data);
         apiShow.className ='api__show'; 
-       
-
     });
-    
 });
-
   function render(data) {
     const { avatar_url, hireable, following, followers, location, name , public_repos , id , login , html_url} = data;
     imgAvatar.src = avatar_url;
-    showUser.innerText = login; 
+    
+    showName.innerText = name;
+    showUser.innerText = ['@' + login]; 
+    
     showUerLogin.href = html_url;
     locations.innerText = location;
     post.innerText = [public_repos + '  ' + 'Repositores'] ;
